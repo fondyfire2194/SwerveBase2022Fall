3d7d8a19ec7142555e7b82839ab8be9d2ee86e92
icemannie <boyle.john65@gmail.com>
2022-10-14 07:25:34 -0500

    Remove Swerve

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -63,11 +63,6 @@ public class Robot extends TimedRobot {
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
 
-    m_robotContainer.m_fieldSim.periodic();
-
-    m_robotContainer.periodic();
-
-    m_robotContainer.m_robotDrive.throttleValue = m_robotContainer.getThrottle();
     
    
   }
@@ -136,8 +131,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void simulationPeriodic() {
-    m_robotContainer.m_fieldSim.periodic();
-    m_robotContainer.simulationPeriodic();
+    
   }
 
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,19 +11,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.Constants.OIConstants;
-import frc.robot.commands.ToggleFieldOriented;
-
-import frc.robot.commands.auto.DriveForward;
-import frc.robot.commands.auto.FiveBallAuto;
-import frc.robot.commands.swerve.JogDriveModule;
-import frc.robot.commands.swerve.JogTurnModule;
-import frc.robot.commands.swerve.SetSwerveDrive;
-import frc.robot.simulation.FieldSim;
-import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.utils.ShuffleboardFieldLocation;
+import frc.robot.commands.Vision.SetDriverMode;
 import frc.robot.utils.ShuffleboardVision;
 
 /*
@@ -34,19 +23,19 @@ import frc.robot.utils.ShuffleboardVision;
  */
 public class RobotContainer {
   // The robot's subsystems
-  final DriveSubsystem m_robotDrive = new DriveSubsystem();
+  
 
   Cameras cams;
 
-  public final FieldSim m_fieldSim = new FieldSim(m_robotDrive);
+  
 
   private final SendableChooser<Command> m_autoChooser = new SendableChooser<Command>();
 
   // The driver's controller
 
-  static Joystick leftJoystick = new Joystick(OIConstants.kDriverControllerPort);
+  static Joystick leftJoystick = new Joystick(0);
 
-  private XboxController m_coDriverController = new XboxController(OIConstants.kCoDriverControllerPort);
+  private XboxController m_coDriverController = new XboxController(1);
 
   final GamepadButtons driver = new GamepadButtons(m_coDriverController, true);
 
@@ -55,95 +44,35 @@ public class RobotContainer {
    */
   public RobotContainer() {
     // Preferences.removeAll();
-    Pref.deleteUnused();
-    Pref.addMissing();
+   
     SmartDashboard.putData("Scheduler", CommandScheduler.getInstance());
     // Configure the button bindings
 
     cams = new Cameras();
-    m_fieldSim.initSim();
-    initializeAutoChooser();
     
     ShuffleboardVision.init(cams);
-    ShuffleboardFieldLocation.init(cams,m_robotDrive);
+    
      
 
     SmartDashboard.putData(CommandScheduler.getInstance());
 
-    PortForwarder.add(5800, "10.21.94.11", 5800);
-    PortForwarder.add(1181, "10.21.94.11", 1181);
-    PortForwarder.add(1182, "10.21.94.11", 1182);
-    PortForwarder.add(1183, "10.21.94,11", 1183);
-    PortForwarder.add(1184, "10.21.94.11", 1184);
+  SmartDashboard.putData("DMON",new SetDriverMode(cams.picam, true));
+  SmartDashboard.putData("DMOFF", new SetDriverMode(cams.picam, false)); 
+  
+  SmartDashboard.putData("DMON", new SetDriverMode(cams.picam, true));
+  SmartDashboard.putData("DMOFF", new SetDriverMode(cams.picam, false));
 
-    // () -> -m_coDriverController.getRawAxis(1),
-    // () -> -m_coDriverController.getRawAxis(0),
-    // () -> -m_coDriverController.getRawAxis(4)));
-    m_robotDrive.setDefaultCommand(
-        new SetSwerveDrive(
-            m_robotDrive,
-            () -> leftJoystick.getRawAxis(1),
-            () -> leftJoystick.getRawAxis(0),
-            () -> leftJoystick.getRawAxis(2)));
 
-    driver.leftTrigger.whileHeld(new JogTurnModule(
-        m_robotDrive,
-        () -> -m_coDriverController.getRawAxis(1),
-        () -> m_coDriverController.getRawAxis(0),
-        () -> m_coDriverController.getRawAxis(2),
-        () -> m_coDriverController.getRawAxis(3)));
-
-    // individual modules
-    driver.leftBumper.whileHeld(new JogDriveModule(
-        m_robotDrive,
-        () -> -m_coDriverController.getRawAxis(1),
-        () -> m_coDriverController.getRawAxis(0),
-        () -> m_coDriverController.getRawAxis(2),
-        () -> m_coDriverController.getRawAxis(3),
-        true));
-
-    // all modules
-    driver.rightBumper.whileHeld(new JogDriveModule(
-        m_robotDrive,
-        () -> -m_coDriverController.getRawAxis(1),
-        () -> m_coDriverController.getRawAxis(0),
-        () -> m_coDriverController.getRawAxis(2),
-        () -> m_coDriverController.getRawAxis(3),
-        false));
 
     JoystickButton button_8 = new JoystickButton(leftJoystick, 8);
     JoystickButton button_7 = new JoystickButton(leftJoystick, 7);
 
-    button_8.whenPressed(new ToggleFieldOriented(m_robotDrive));
-    // position turn modules individually
-    // driver.X_button.whenPressed(new PositionTurnModule(m_robotDrive,
-    // ModulePosition.FRONT_LEFT));
-    // driver.A_button.whenPressed(new PositionTurnModule(m_robotDrive,
-    // ModulePosition.FRONT_RIGHT));
-    // driver.B_button.whenPressed(new PositionTurnModule(m_robotDrive,
-    // ModulePosition.BACK_LEFT));
-    // driver.Y_button.whenPressed(new PositionTurnModule(m_robotDrive,
-    // ModulePosition.BACK_RIGHT));
-
+  
   }
 
-  private void initializeAutoChooser() {
-    m_autoChooser.setDefaultOption("Do Nothing", new WaitCommand(0));
-    m_autoChooser.addOption("Drive Forward", new DriveForward(m_robotDrive));
-    m_autoChooser.addOption("5 Ball Auto", new FiveBallAuto(m_robotDrive));
+ 
 
-    SmartDashboard.putData("Auto Selector", m_autoChooser);
-
-  }
-
-  public void simulationPeriodic() {
-    m_fieldSim.periodic();
-    periodic();
-  }
-
-  public void periodic() {
-    m_fieldSim.periodic();
-  }
+ 
 
   public double getThrottle() {
     return -leftJoystick.getThrottle();
